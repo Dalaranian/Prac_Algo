287b8dc4a045be5100198cfba0f06ffdb3d7feb3
J.M. Bae <78770745+Dalaranian@users.noreply.github.com>
2023-03-17 10:30:11 +0900

    [level 2] Title: 올바른 괄호, Time: 13.07 ms, Memory: 54.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,17 @@
+import java.util.LinkedList;
+class Solution {
+    boolean solution(String s) {
+        LinkedList<Character> list = new LinkedList<>();
+        for(char c : s.toCharArray()){
+            if(c == '('){
+                list.add(c);
+            }else{
+                if(list.isEmpty()){
+                    return false;
+                }
+                list.pollLast();
+            }
+        }
+        return list.isEmpty();
+    }
+}
