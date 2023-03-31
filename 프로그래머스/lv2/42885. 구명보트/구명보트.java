a011c43edd898ff91301f53b7bcabd163231c498
J.M.Bae <78770745+Dalaranian@users.noreply.github.com>
2023-03-31 11:27:18 +0900

    [level 2] Title: 구명보트, Time: 9.64 ms, Memory: 53.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.java
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.java
@@ -3,15 +3,16 @@ import java.util.*;
 class Solution {
     public int solution(int[] people, int limit) {
         Arrays.sort(people);
-        int answer = 0;
-        int left = 0, right = people.length - 1;
-        while (left <= right) {
-            if (people[left] + people[right] <= limit) {
-                left++;
+        int min = 0;
+        int max = people.length - 1;
+        int boat = 0;
+        while(min <= max){
+            if(people[min] + people[max] <= limit){
+                min ++;
             }
-            right--;
-            answer++;
+            max--;
+            boat++;
         }
-        return answer;
+        return boat;
     }
 }
