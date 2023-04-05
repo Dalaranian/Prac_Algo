baefcaca47fad2218181ac4cf3d2119aaa569a34
J.M.Bae <78770745+Dalaranian@users.noreply.github.com>
2023-04-05 12:48:00 +0900

    [level 0] Title: 배열 두배 만들기, Time: 3.17 ms, Memory: 94.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.java
+++ 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.java
@@ -2,7 +2,6 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] numbers) {
-        int[] answer = Arrays.stream(numbers).map(n -> n * 2).toArray();
-        return answer;
+        return Arrays.stream(numbers).map(n -> n * 2).toArray();
     }
 }
