05ab62c1892b3b80552a2258f58bfbb5d929a210
J.M.Bae <78770745+Dalaranian@users.noreply.github.com>
2023-04-16 20:45:32 +0900

    [level 1] Title: K번째수, Time: 0.37 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.java
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -2,21 +2,34 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-             Queue<Integer> st = new LinkedList<>();
+        Queue<Integer> st = new LinkedList<>();
         for (int[] temp : commands) {
             System.out.println(Arrays.toString(temp));
             List<Integer> list = new ArrayList<>();
             int start = temp[0];
             int end = temp[1];
-            for(int i = start-1;i<=end -1;i++){
+            for (int i = start - 1; i <= end - 1; i++) {
                 list.add(array[i]);
             }
-            //나중에 정렬알고리즘 짜야지
-            Collections.sort(list);
-            st.add(list.get(temp[2]-1));
+
+            // 선택정렬
+            for (int i = 0; i < list.size() - 1; i++) {
+                int minIndex = i;
+                for (int j = i + 1; j < list.size(); j++) {
+                    if (list.get(j) < list.get(minIndex)) {
+                        minIndex = j;
+                    }
+                }
+                int tempNum = list.get(minIndex);
+                list.set(minIndex, list.get(i));
+                list.set(i, tempNum);
+            }
+
+            st.add(list.get(temp[2] - 1));
         }
+
         int[] answer = new int[commands.length];
-        for(int i = 0;i<=answer.length-1;i++){
+        for (int i = 0; i <= answer.length - 1; i++) {
             answer[i] = st.poll();
         }
         return answer;
