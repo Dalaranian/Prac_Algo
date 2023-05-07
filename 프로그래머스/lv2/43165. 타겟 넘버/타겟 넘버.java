187096c355977f2548ca8318bac4b355a63b4363
J.M.Bae <78770745+Dalaranian@users.noreply.github.com>
2023-05-07 20:22:46 +0900

    [level 2] Title: 타겟 넘버, Time: 0.45 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,20 +1,25 @@
 class Solution {
     public int solution(int[] numbers, int target) {
-        return dfs(numbers, target, 0, 0);
+        int answer = 0;
+        boolean[] visited = new boolean[numbers.length];
+        answer = dfs(numbers, target, visited, 0, 0);
+        return answer;
     }
-    
-    private int dfs(int[] numbers, int target, int index, int sum) {
-        if (index == numbers.length) { // 모든 숫자를 다 처리한 경우
-            if (sum == target) { // target과 일치하는 경우
+
+    private int dfs(int[] numbers, int target, boolean[] visited, int index, int sum) {
+        if (index == numbers.length) {
+            if (sum == target) {
                 return 1;
             }
             return 0;
         }
-        
-        // 현재 인덱스의 숫자를 더할 경우와 뺄 경우 각각에 대해 재귀 호출
-        int plus = dfs(numbers, target, index + 1, sum + numbers[index]);
-        int minus = dfs(numbers, target, index + 1, sum - numbers[index]);
-        
-        return plus + minus;
+
+        int count = 0;
+        visited[index] = true;
+        count += dfs(numbers, target, visited, index + 1, sum + numbers[index]);
+        count += dfs(numbers, target, visited, index + 1, sum - numbers[index]);
+        visited[index] = false;
+
+        return count;
     }
 }
