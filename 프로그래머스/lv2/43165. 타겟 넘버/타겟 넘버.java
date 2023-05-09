9141a47c2eb39b297816b0b7fcd8244d42417ccf
J.M.Bae <78770745+Dalaranian@users.noreply.github.com>
2023-05-09 23:35:34 +0900

    [level 2] Title: 타겟 넘버, Time: 6.44 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,25 +1,35 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] numbers, int target) {
         int answer = 0;
-        boolean[] visited = new boolean[numbers.length];
-        answer = dfs(numbers, target, visited, 0, 0);
+        Stack<Node> stack = new Stack<>();
+        stack.push(new Node(0, 0));
+
+        while (!stack.isEmpty()) {
+            Node node = stack.pop();
+
+            if (node.index == numbers.length) {
+                if (node.sum == target) {
+                    answer++;
+                }
+                continue;
+            }
+
+            stack.push(new Node(node.index + 1, node.sum + numbers[node.index]));
+            stack.push(new Node(node.index + 1, node.sum - numbers[node.index]));
+        }
+
         return answer;
     }
 
-    private int dfs(int[] numbers, int target, boolean[] visited, int index, int sum) {
-        if (index == numbers.length) {
-            if (sum == target) {
-                return 1;
-            }
-            return 0;
+    class Node {
+        int index;
+        int sum;
+
+        Node(int index, int sum) {
+            this.index = index;
+            this.sum = sum;
         }
-
-        int count = 0;
-        visited[index] = true;
-        count += dfs(numbers, target, visited, index + 1, sum + numbers[index]);
-        count += dfs(numbers, target, visited, index + 1, sum - numbers[index]);
-        visited[index] = false;
-
-        return count;
     }
 }
