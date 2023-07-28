307fe85a4f653e93c6fef10276bfecf27ffb61a5
J.M.Bae <78770745+Dalaranian@users.noreply.github.com>
2023-07-29 00:31:04 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 1.08 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -3,20 +3,25 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
-class Solution {
+public class Solution {
+
     public int solution(int n, int k) {
         int answer = 0;
 
+        // K 진수로 변환
         String baseConversion = Integer.toString(n, k);
-        
-        StringTokenizer tokenizer = new StringTokenizer(baseConversion, "0");
 
+        // 요구사항에 맞게 토큰화
+        StringTokenizer tokenizer = new StringTokenizer(baseConversion, "0");
+        
         List<String> list = new ArrayList<>();
 
+        // 토큰들을 List 에 저장
         while (tokenizer.hasMoreTokens()) {
             list.add(tokenizer.nextToken());
         }
 
+        // BigInteger 를 활용한 소수 구하기
         for (String str : list) {
             BigInteger num = BigInteger.valueOf(Long.parseLong(str));
             if (num.isProbablePrime(100)) {
