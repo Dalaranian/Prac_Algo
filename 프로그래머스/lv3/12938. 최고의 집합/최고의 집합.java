f534d26fe8830a1404b2d256d5a30289eb0720d9
J.M.Bae <78770745+Dalaranian@users.noreply.github.com>
2023-09-19 23:43:14 +0900

    [level 3] Title: 최고의 집합, Time: 0.01 ms, Memory: 52.4 MB -BaekjoonHub

--- 프로그래머스/lv3/12938. 최고의 집합/최고의 집합.java
+++ 프로그래머스/lv3/12938. 최고의 집합/최고의 집합.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.Arrays;
 
 class Solution {
@@ -7,14 +6,11 @@ class Solution {
             // 종료조건 : 불가능할 경우
             return new int[]{-1};
         }
-
-        int start = s / n;
-        int remain = s % n;
-
         int[] result = new int[n];
-        Arrays.fill(result, start);
+        Arrays.fill(result, (s / n));
 
         int index = 0;
+        int remain = s % n;
         while (remain > 0) {
             if (index >= n) {
                 index = 0;
