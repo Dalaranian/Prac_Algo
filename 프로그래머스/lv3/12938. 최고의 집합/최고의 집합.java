6229f99cea37c039aa00c0dc5dea2365677d08c6
J.M.Bae <78770745+Dalaranian@users.noreply.github.com>
2023-09-19 23:46:14 +0900

    Update 최고의 집합.java

--- 프로그래머스/lv3/12938. 최고의 집합/최고의 집합.java
+++ 프로그래머스/lv3/12938. 최고의 집합/최고의 집합.java
@@ -6,17 +6,16 @@ class Solution {
             // 종료조건 : 불가능할 경우
             return new int[]{-1};
         }
-        
+        // 배열 준비
         int[] result = new int[n];
         Arrays.fill(result, (s / n));
-
+        // 로직 구현
         int index = 0;
         int remain = s % n;
         while (remain > 0) {
             if (index >= n) {
                 index = 0;
             }
-
             result[index++]++;
             remain--;
         }
