58bd2be93bcd685b4b2681eec935cbc60ad1b413
J.M.Bae <78770745+Dalaranian@users.noreply.github.com>
2023-09-22 23:34:54 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.18 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] id_list, String[] report, int k) {
+        Map<String, HashSet<String>> reportMap = generateReportMap(id_list);
+        Map<String, Integer> index = getIndex(id_list);
+        for (String s : report) {
+            String[] currentReport = s.split(" ");
+            reportMap.get(currentReport[1]).add(currentReport[0]);
+        }
+        int[] result = new int[id_list.length];
+        for (String s : reportMap.keySet()) {
+            HashSet<String> currentReportMember = reportMap.get(s);
+            if(currentReportMember.size() >= k){
+                for (String reportMember : currentReportMember) {
+                    result[index.get(reportMember)]++;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private Map<String, Integer> getIndex(String[] idList) {
+        int currentIndex = 0;
+        Map<String, Integer> result = new HashMap<>();
+        for (String s : idList) {
+            result.put(s, currentIndex);
+            currentIndex++;
+        }
+        return result;
+    }
+
+    private HashMap<String, HashSet<String>> generateReportMap(String[] idList) {
+        HashMap<String, HashSet<String>> result = new HashMap<>();
+        for (String key : idList) {
+            result.put(key, new HashSet<>());
+        }
+        return result;
+    }
+}
