f4e14acf4017672d93cc5bbd364ba9e0c9dd6619
J.M.Bae <78770745+Dalaranian@users.noreply.github.com>
2023-09-22 23:37:59 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.11 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -2,12 +2,14 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
-        Map<String, HashSet<String>> reportMap = generateReportMap(id_list);
-        Map<String, Integer> index = generateIndex(id_list);
+        Map<String, HashSet<String>> reportMap = getReportMap(id_list);
+        Map<String, Integer> index = getIndex(id_list);
+
         for (String s : report) {
             String[] currentReport = s.split(" ");
             reportMap.get(currentReport[1]).add(currentReport[0]);
         }
+
         int[] result = new int[id_list.length];
         for (String s : reportMap.keySet()) {
             HashSet<String> currentReportMember = reportMap.get(s);
@@ -21,7 +23,7 @@ class Solution {
         return result;
     }
 
-    private Map<String, Integer> generateIndex(String[] idList) {
+    private Map<String, Integer> getIndex(String[] idList) {
         int currentIndex = 0;
         Map<String, Integer> result = new HashMap<>();
         for (String s : idList) {
@@ -31,7 +33,7 @@ class Solution {
         return result;
     }
 
-    private HashMap<String, HashSet<String>> generateReportMap(String[] idList) {
+    private HashMap<String, HashSet<String>> getReportMap(String[] idList) {
         HashMap<String, HashSet<String>> result = new HashMap<>();
         for (String key : idList) {
             result.put(key, new HashSet<>());
