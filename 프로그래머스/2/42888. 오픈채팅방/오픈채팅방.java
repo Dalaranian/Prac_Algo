a32a20dd8594d4f080f81fd7c459bec9ec29f5e8
J.M.Bae <78770745+Dalaranian@users.noreply.github.com>
2025-02-06 15:23:03 +0900

    [level 2] Title: 오픈채팅방, Time: 242.32 ms, Memory: 172 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -1,4 +1,5 @@
 import java.util.*;
+
 import java.util.stream.Collectors;
 
 class Solution {
@@ -8,18 +9,16 @@ class Solution {
                 .map(row -> row.split(" "))
                 .filter(row -> !row[0].equals("Leave")) // 떠나는 것은 닉네임 변경과 연관 없음
                 .collect(Collectors.toMap(
-                        row -> row[1], // UID
-                        row -> row[2], // Nickname
-                        (oldKey, newKey) -> newKey
+                        row -> row[1], // UID       (Key)
+                        row -> row[2], // Nickname  (Value)
+                        (oldKey, newKey) -> newKey // 키 중복 시 신규 키로 덮어쓰기
                 ));
 
         // 로그 메세지 생성 후 반환
         return Arrays.stream(record)
                 .map(row -> row.split(" "))
-                .filter(row -> !row[0].equals("Change"))
-                .map(row -> {
-                    return userInfo.get(row[1]) + (row[0].equals("Enter") ?  "님이 들어왔습니다." : "님이 나갔습니다.");
-                })
+                .filter(row -> !row[0].equals("Change")) // 변경 이벤트는 로그 생성하지 않음
+                .map(row -> userInfo.get(row[1]) + (row[0].equals("Enter") ?  "님이 들어왔습니다." : "님이 나갔습니다."))
                 .toArray(String[]::new);
     }
 }
